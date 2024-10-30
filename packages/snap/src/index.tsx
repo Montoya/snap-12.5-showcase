4cbe3d97961f03a64391ef92827887ec98da26b5
Christian Montoya <christian.montoya@consensys.net>
2024-10-30 17:36:30 -0400

    v1

--- packages/snap/src/index.tsx
+++ packages/snap/src/index.tsx
@@ -1,5 +1,42 @@
-import type { OnRpcRequestHandler } from '@metamask/snaps-sdk';
-import { Box, Text, Bold } from '@metamask/snaps-sdk/jsx';
+import {
+  UserInputEventType,
+  type OnRpcRequestHandler,
+  type OnUserInputHandler,
+  type OnHomePageHandler,
+} from "@metamask/snaps-sdk";
+import { Container, Box, Section, Heading, Text, Bold, Footer, Button, Icon, Image, Link } from '@metamask/snaps-sdk/jsx';
+import arrow from "./../img/arrow.svg"
+
+export const onHomePage: OnHomePageHandler = async () => {
+  return {
+    content: (
+      <Box>
+        <Section>
+          <Heading>Hello world!</Heading>
+          <Text>Below is a showcase of features in MetaMask Extension 12.5:</Text>
+        </Section>
+        <Section>
+          <Heading>New Icon component!</Heading>
+          <Icon name="wifi"></Icon>
+          <Icon name="card" color="muted"></Icon>
+          <Icon name="home"></Icon>
+        </Section>
+        <Section>
+          <Text>Buttons can now wrap images and icons!</Text>
+          <Box>
+            <Button><Image src={arrow} /></Button>
+          </Box>
+          <Box>
+            <Button><Icon name="add"></Icon></Button>
+          </Box>
+        </Section>
+        <Section>
+          <Text>Learn more in our <Link href="https://docs.metamask.io/snaps/">documentation</Link>.</Text>
+        </Section>
+      </Box>
+    ),
+  };
+};
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -17,27 +54,79 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
 }) => {
   switch (request.method) {
     case 'hello':
-      return snap.request({
+      const result = await snap.request({
         method: 'snap_dialog',
         params: {
-          type: 'confirmation',
           content: (
-            <Box>
-              <Text>
-                Hello, <Bold>{origin}</Bold>!
-              </Text>
-              <Text>
-                This custom confirmation is just for display purposes.
-              </Text>
-              <Text>
-                But you can edit the snap source code to make it do something,
-                if you want to!
-              </Text>
-            </Box>
+            <Container>
+              <Box>
+                <Section>
+                  <Heading>
+                    Hello, {origin}!
+                  </Heading>
+                  <Text>
+                    This custom confirmation is just for display purposes. It uses the new Section component to create a light background section within the page. Do you like it?
+                  </Text>
+                </Section>
+              </Box>
+              <Footer>
+                <Button name="no">No</Button>
+                <Button name="yes">Yes</Button>
+              </Footer>
+            </Container>
           ),
         },
       });
+
+      console.log(result); 
+
+      return snap.request({
+        method: 'snap_dialog',
+        params: { 
+          content: (
+            <Container>
+              <Box>
+                <Section>
+                  <Heading>Response received</Heading>
+                  <Text>You said {''+result}!</Text>
+                </Section>
+              </Box>
+            </Container>
+          )
+        },
+      }); 
     default:
       throw new Error('Method not found.');
+  }
+};
+
+export const onUserInput: OnUserInputHandler = async ({ id, event }) => {
+  if (event.type === UserInputEventType.ButtonClickEvent) {
+    switch (event.name) {
+      case "no": // User selected "No" in the footer.
+        await snap.request({
+          method: "snap_resolveInterface",
+          params: {
+            id,
+            value: "No",
+          },
+        });
+        break;
+
+      case "yes": {
+        // User selected "Yes" in the footer
+        await snap.request({
+          method: "snap_resolveInterface",
+          params: {
+            id,
+            value: "Yes",
+          },
+        });
+        break;
+      }
+
+      default:
+        break;
+    }
   }
 };
